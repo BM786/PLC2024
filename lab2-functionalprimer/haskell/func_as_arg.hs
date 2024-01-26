cba6f05e73299df481319ccbeabdddb8d891f7e2
BM 786 <220065665@aston.ac.uk>
2024-01-26 17:50:37 +0000

    func_as_arg.hs conpleted

--- lab2-functionalprimer/haskell/func_as_arg.hs
+++ lab2-functionalprimer/haskell/func_as_arg.hs
@@ -1,9 +1,23 @@
---create inpFunc
-inpFunc = [1..5] 
+-- create inpFunc with a range [a..b]
+inpFunc :: [Int]
+inpFunc = [1..5]
 
---Define applicatorFunc
-applicatorFunc inpFunc s = if s=='s' then sum inpFunc else (sum inpFunc)/5  
+-- Define applicatorFunc to accept any list of integers and a flag 's' for sum, 'a' for average
+applicatorFunc :: [Int] -> Char -> Double
+applicatorFunc inpFunc flag
+  | flag == 's' = fromIntegral $ sum inpFunc
+  | flag == 'a' = fromIntegral (sum inpFunc) / fromIntegral (length inpFunc)
+  | otherwise   = error "Invalid flag"
 
+main :: IO ()
 main = do
-    let result = applicatorFunc inpFunc 'a' --Call applicatorFunc with inpFunc and 'a' as args
-    putStrLn("sum = " ++ show(result))
+  let result = applicatorFunc inpFunc 'a' -- Call applicatorFunc with inpFunc and 'a' as args
+  putStrLn $ case resultType of
+    's' -> "Sum = " ++ show result
+    'a' -> "Average = " ++ show result
+    _   -> "Invalid result type"
+
+-- Define the type of result you want ('s' for sum, 'a' for average)
+resultType :: Char
+resultType = 'a'
+    
