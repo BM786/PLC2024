009aec06d8987be6f0b06f9791d608b4f84c8664
BM 786 <220065665@aston.ac.uk>
2024-01-26 17:26:25 +0000

    task2 done

--- lab2-functionalprimer/haskell/task2.hs
+++ lab2-functionalprimer/haskell/task2.hs
@@ -1,4 +1,12 @@
+module Main where
+
+onePlusOne :: IO ()
+onePlusOne = putStrLn ("1 + 1 = 2")
+
+main :: IO ()
+
 main = do
     putStrLn "Welcome to the programme. Please enter your name"
     name <- getLine
     putStrLn("Hello " ++ name ++ ", hope you like Haskell.")
+    onePlusOne
